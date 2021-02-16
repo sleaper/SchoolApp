cd1e6c63a3335739b6ae544edebc32906c97c213
Petr <petrik.spac@gmail.com>
2021-02-16 13:30:09 +0100

    IOS rename

--- ios/skolaOfflineTests/skolaOfflineTests.m
+++ ios/skolaOfflineTests/skolaOfflineTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mobileAppTests : XCTestCase
+@interface skolaOfflineTests : XCTestCase
 
 @end
 
-@implementation mobileAppTests
+@implementation skolaOfflineTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
