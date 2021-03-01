26c549303b74b071c7c003ce6e0bb6e881b86360
Petr <petrik.spac@gmail.com>
2021-03-01 11:52:12 +0100

    Hopefully renamed IOS native files

--- ios/skolaOfflineTests/skolaOfflineTests.m
+++ ios/skolaOfflineTests/skolaOfflineTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface skolaOfflineTests : XCTestCase
+@interface skolaofflineTests : XCTestCase
 
 @end
 
-@implementation skolaOfflineTests
+@implementation skolaofflineTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
