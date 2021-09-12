151eed437cd4819843b213ba120d2405a6fa5c6c
Rambo <petrik.spac@gmail.com>
2021-09-12 16:05:00 +0200

    Authtabs theme fix

--- src/AuthTabs.tsx
+++ src/AuthTabs.tsx
@@ -1,9 +1,13 @@
 import React, {useContext} from 'react';
 import Login from './components/login';
 import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
 import {ThemeContext} from './components/theme/ThemeProvider';
-import {lightTheme, darkTheme} from './components/theme/Themes';
+//import {lightTheme, darkTheme} from './components/theme/Themes';
 
 const Stack = createStackNavigator();
 
@@ -11,7 +15,7 @@ export default function AuthTabs() {
   const {theme} = useContext(ThemeContext);
 
   return (
-    <NavigationContainer theme={theme === 'dark' ? darkTheme : lightTheme}>
+    <NavigationContainer theme={theme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack.Navigator
         screenOptions={{headerStatusBarHeight: 0, headerShown: false}}>
         <Stack.Screen name="Login" component={Login} />
