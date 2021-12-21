f760765286a586395319c145b0e81efa9cb8976a
Rambo <petrik.spac@gmail.com>
2021-12-21 21:38:17 +0100

    day modal

--- src/components/Calendar/Day.tsx
+++ src/components/Calendar/Day.tsx
@@ -4,15 +4,7 @@ import DayItem from './DayItem';
 import HTMLView from 'react-native-htmlview';
 import Emoji from 'react-native-emoji';
 import {NetworkStatus} from '@apollo/client';
-import {
-  Button,
-  Center,
-  Flex,
-  Modal,
-  ScrollView,
-  Text,
-  useColorMode,
-} from 'native-base';
+import {Center, Flex, Modal, ScrollView, useColorMode} from 'native-base';
 import {ActivityIndicator, FlatList} from 'react-native';
 import {useCalendarDayQuery} from './Day.codegen';
 import Arrows from './Arrows';
@@ -101,6 +93,7 @@ export default function Day({route}) {
           mt={15}
           mb={25}
           maxWidth="400px">
+          <Modal.CloseButton />
           <Modal.Header>{modalTitle}</Modal.Header>
           <Modal.Body>
             <ScrollView>
@@ -114,19 +107,6 @@ export default function Day({route}) {
               />
             </ScrollView>
           </Modal.Body>
-
-          <Modal.Footer>
-            <Button
-              borderRadius={'20px'}
-              padding="10px"
-              onPress={() => {
-                setModalVisible(!modalVisible);
-              }}>
-              <Text fontWeight="bold" textAlign="center">
-                Hide Modal
-              </Text>
-            </Button>
-          </Modal.Footer>
         </Modal.Content>
       </Modal>
 
@@ -146,101 +126,3 @@ export default function Day({route}) {
     </Flex>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   rowContainer: {
-//     height: 100,
-//     width: '90%',
-//     marginLeft: 20,
-//     marginTop: 10,
-//     borderRadius: 20,
-//     paddingLeft: 15,
-//     paddingTop: 8,
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//   },
-//   teacher: {
-//     marginTop: 2.5,
-//     fontSize: 14,
-//     color: '#777',
-//   },
-//   rowText: {
-//     flex: 6,
-//     paddingTop: 5,
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//   },
-//   time: {
-//     flex: 1,
-//     textAlign: 'center',
-//   },
-//   title: {
-//     paddingLeft: 20,
-//     paddingTop: 10,
-//     fontSize: 20,
-//     fontWeight: 'bold',
-//   },
-//   arrows: {
-//     justifyContent: 'space-between',
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//   },
-//   number: {
-//     fontSize: 25,
-//     paddingTop: 8,
-//   },
-//   subject: {
-//     fontSize: 16,
-//     fontWeight: 'bold',
-//   },
-//   dateText: {
-//     fontSize: 20,
-//     fontWeight: 'bold',
-//   },
-//   centeredView: {
-//     flex: 1,
-//     justifyContent: 'flex-end',
-//     alignItems: 'center',
-//     marginTop: 22,
-//     marginBottom: 50,
-//   },
-//   modalView: {
-//     width: '90%',
-//     height: 250,
-//     backgroundColor: 'white',
-//     padding: 15,
-//     alignItems: 'flex-start',
-//     shadowColor: '#000',
-//     shadowOffset: {
-//       width: 0,
-//       height: 2,
-//     },
-//     shadowOpacity: 0.25,
-//     shadowRadius: 3.84,
-//     elevation: 5,
-//     borderRadius: 20,
-//     borderWidth: 1,
-//     borderColor: '#FFFF',
-//   },
-//   openButton: {
-//     backgroundColor: '#F194FF',
-//     borderRadius: 20,
-//     padding: 10,
-//     elevation: 2,
-//     position: 'absolute',
-//     right: 30,
-//     bottom: 30,
-//   },
-//   textStyle: {
-//     color: 'white',
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//   },
-//   modalText: {
-//     marginBottom: 15,
-//     textAlign: 'center',
-//   },
-// });
